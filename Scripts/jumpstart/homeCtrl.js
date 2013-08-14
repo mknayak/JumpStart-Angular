a782c0b72cd56d165a4dc069a08e65df212e4fa2
mknayak <manas.nayak@outlook.com>
2013-08-14 10:17:14 +0530

    initial check in

---
+++ Scripts/jumpstart/homeCtrl.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="../angular.js" />
+var myApp = angular.module("MyApp", []).config([
+    '$routeProvider', function ($routeProvider) {
+
+        $routeProvider.when('/srcHtml', {templateUrl:'src/html.html'})
+
+
+    }]);
+
+
+
+function HomeCtrl($scope) {
+    $scope.show = false;
+    $scope.display = function () {
+        $scope.show = $scope.show ? false : true;
+    };
+}
